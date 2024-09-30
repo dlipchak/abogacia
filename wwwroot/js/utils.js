8ce93ee1be8856184e811c103d1229139d8732e9
dlipchak <daniel.lipchak7603@gmail.com>
2024-09-30 16:57:53 -0300

    cambios

--- wwwroot/js/utils.js
+++ wwwroot/js/utils.js
@@ -1,3 +1,3 @@
 ﻿function currencyFormat(num) {
-    return '$' + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  return "$" + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
 }
